be85f58c1c77ab8e37b83505f8f29a0389c5618f
djb <577625177@qq.com>
2024-12-01 21:49:58 -0500

    add skiplink and update resopnsive nav

--- final/final-education/src/Header/Header.jsx
+++ final/final-education/src/Header/Header.jsx
@@ -6,8 +6,9 @@ function Header({ changePage }) {
     return (
         <>
             <header className="header">
+                <a href="#main" className='skip-link'>SKip to main</a>
                 <div className="header-top">
-                    <a href="/" className="img-link">
+                    <a href="/" className="img-link" aria-label="logo">
                         <img src="/logo.jpg" alt="a woman in growing graduation" className="logo-img" />
                     </a>
                     <h1 className="header-title">Welcome To Edu's World !</h1>
