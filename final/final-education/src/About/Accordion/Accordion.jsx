7d9aa6bd4b0aff0b661615388eb0da01c4e15ec7
djb <577625177@qq.com>
2024-12-05 20:33:51 -0500

    update license

--- final/final-education/src/About/Accordion/Accordion.jsx
+++ final/final-education/src/About/Accordion/Accordion.jsx
@@ -21,7 +21,7 @@ function Accordion({ data }) {
                     a11yLabel="toggle accordion item"
                 >
                     {question}
-                    <svg className="icon" xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 -960 960 960" width="16px" >
+                    <svg className="arrow-icon" xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 -960 960 960" width="16px" >
                         <path d="M444-768v438L243-531l-51 51 288 288 288-288-51-51-201 201v-438h-72Z" />
                     </svg>
                 </Button>
